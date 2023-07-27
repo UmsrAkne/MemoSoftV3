a3026ab07779981a508950e487913093f5f6884c
Akn <sibata486@gmail.com>
2023-07-27 22:38:16 +0900

    add feat / タグをテーブルに追加する機能を実装

--- MemoSoftV3/Models/DatabaseManager.cs
+++ MemoSoftV3/Models/DatabaseManager.cs
@@ -1,4 +1,6 @@
-﻿namespace MemoSoftV3.Models
+﻿using System.Linq;
+
+namespace MemoSoftV3.Models
 {
     public class DatabaseManager
     {
@@ -12,6 +14,24 @@
         public void Add(Comment cm)
         {
             DataSource.Add(cm);
+        }
+
+        /// <summary>
+        ///     テーブルにタグを追加します。
+        ///     tag.Name が重複するタグが入力された場合、tag.Name が空文字か null だった場合は、追加せずにメソッドを終了します。
+        /// </summary>
+        /// <param name="tag">追加するタグです。</param>
+        public void Add(Tag tag)
+        {
+            var isEmpty = string.IsNullOrEmpty(tag.Name);
+            var containsSame = DataSource.GetTags().Any(t => t.Name == tag.Name);
+
+            if (isEmpty || containsSame)
+            {
+                return;
+            }
+
+            DataSource.Add(tag);
         }
     }
 }
--- MemoSoftV3Tests/Models/DatabaseManagerTest.cs
+++ MemoSoftV3Tests/Models/DatabaseManagerTest.cs
@@ -15,5 +15,41 @@ namespace MemoSoftV3Tests.Models
 
             Assert.That(source.GetComments().First(), Is.EqualTo(comment));
         }
+
+        [Test]
+        public void AddTagTest_正常系()
+        {
+            var source = new DatabaseMock();
+            var manager = new DatabaseManager(source);
+            var tag = new Tag() { Id = 1, Name = "a", };
+            manager.Add(tag);
+
+            Assert.That(source.GetTags().First(), Is.EqualTo(tag));
+        }
+
+        [Test]
+        public void AddTagTest_空文字入力()
+        {
+            var source = new DatabaseMock();
+            var manager = new DatabaseManager(source);
+            var tag = new Tag() { Id = 1, Name = string.Empty, };
+            manager.Add(tag);
+
+            Assert.IsNull(source.GetTags().FirstOrDefault());
+        }
+
+        [Test]
+        public void AddTagTest_重複入力()
+        {
+            var source = new DatabaseMock();
+            var manager = new DatabaseManager(source);
+            var tag1 = new Tag() { Id = 1, Name = "a", };
+            var tag2 = new Tag() { Id = 2, Name = "a", };
+            manager.Add(tag1);
+            manager.Add(tag2);
+
+            Assert.That(source.GetTags().First(), Is.EqualTo(tag1), "tag1 が先に追加されるので、tag2 は入力されない。");
+            Assert.That(source.GetTags().Count(), Is.EqualTo(1), "入力は２回だが、実際に入力されるのは１回目のみ");
+        }
     }
 }
