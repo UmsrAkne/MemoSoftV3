aad7cccaa4b8e6943b89e1305d4d2dfa121331dd
Akn <sibata486@gmail.com>
2023-07-27 22:20:31 +0900

    add files / テストプロジェクトを追加

---
+++ MemoSoftV3Tests/Models/DatabaseManagerTest.cs
@@ -0,0 +1,19 @@
+﻿using MemoSoftV3.Models;
+
+namespace MemoSoftV3Tests.Models
+{
+    [TestFixture]
+    public class DatabaseManagerTest
+    {
+        [Test]
+        public void AddCommentTest()
+        {
+            var source = new DatabaseMock();
+            var manager = new DatabaseManager(source);
+            var comment = new Comment();
+            manager.Add(comment);
+
+            Assert.That(source.GetComments().First(), Is.EqualTo(comment));
+        }
+    }
+}
---
+++ MemoSoftV3Tests/Models/DatabaseMock.cs
@@ -0,0 +1,43 @@
+﻿using MemoSoftV3.Models;
+
+namespace MemoSoftV3Tests.Models
+{
+    public class DatabaseMock : IDataSource
+    {
+        private List<Comment> Comments { get; } = new ();
+
+        private List<Tag> Tags { get; } = new ();
+
+        private List<TagMap> TagMaps { get; } = new ();
+
+        public IEnumerable<Comment> GetComments()
+        {
+            return Comments;
+        }
+
+        public IEnumerable<Tag> GetTags()
+        {
+            return Tags;
+        }
+
+        public IEnumerable<TagMap> GetTagMaps()
+        {
+            return TagMaps;
+        }
+
+        public void Add(Comment cm)
+        {
+            Comments.Add(cm);
+        }
+
+        public void Add(Tag tag)
+        {
+            Tags.Add(tag);
+        }
+
+        public void Add(TagMap tagMap)
+        {
+            TagMaps.Add(tagMap);
+        }
+    }
+}
