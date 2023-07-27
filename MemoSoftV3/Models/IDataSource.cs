dfa30bf5129a20163621c95c4c72d196f42eb8bb
Akn <sibata486@gmail.com>
2023-07-27 22:06:59 +0900

    add feat / IDataSource にタグを扱う機能を実装

--- MemoSoftV3/Models/IDataSource.cs
+++ MemoSoftV3/Models/IDataSource.cs
@@ -6,6 +6,14 @@ namespace MemoSoftV3.Models
     {
         public IEnumerable<Comment> GetComments();
 
+        public IEnumerable<Tag> GetTags();
+
+        public IEnumerable<TagMap> GetTagMaps();
+
         public void Add(Comment cm);
+
+        public void Add(Tag tag);
+
+        public void Add(TagMap tagMap);
     }
 }
