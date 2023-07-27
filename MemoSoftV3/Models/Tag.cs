45d8706b42155f3a2d5690216acef4b4b05b435d
Akn <sibata486@gmail.com>
2023-07-27 22:06:43 +0900

    add files / タグに関するクラスを追加

---
+++ MemoSoftV3/Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemoSoftV3.Models
+{
+    public class Tag
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ MemoSoftV3/Models/TagMap.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemoSoftV3.Models
+{
+    public class TagMap
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int TagId { get; set; }
+
+        [Required]
+        public int CommentId { get; set; }
+    }
+}
