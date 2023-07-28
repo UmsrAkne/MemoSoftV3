7843ceab0099f1f653972d79153717e1e033be37
Akn <sibata486@gmail.com>
2023-07-28 19:34:28 +0900

    add files / Database に対して行った操作を表すクラスを追加

---
+++ MemoSoftV3/Models/DatabaseAction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MemoSoftV3.Models
+{
+    public class DatabaseAction
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime DateTime { get; set; } = DateTime.Now;
+
+        [Required]
+        public Kind Kind { get; set; }
+
+        [Required]
+        public Target Target { get; set; }
+
+        [Required]
+        public int TargetId { get; set; }
+    }
+}
