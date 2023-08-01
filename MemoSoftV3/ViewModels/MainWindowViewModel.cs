540a34a115fa68d180e7cebcfd3940ee23e1ef8d
Akn <sibata486@gmail.com>
2023-08-01 15:20:32 +0900

    add / MainWindowViewModel に最低限のコマンドとプロパティを実装

--- MemoSoftV3/ViewModels/MainWindowViewModel.cs
+++ MemoSoftV3/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,36 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.ObjectModel;
+using MemoSoftV3.Models;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace MemoSoftV3.ViewModels
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     public class MainWindowViewModel : BindableBase
     {
         private string title = "Prism Application";
 
+        public MainWindowViewModel()
+        {
+            LoadCommand.Execute();
+        }
+
         public string Title { get => title; set => SetProperty(ref title, value); }
+
+        public ObservableCollection<Comment> Comments { get; private set; }
+
+        private DatabaseManager DatabaseManager { get; set; }
+
+        private DelegateCommand LoadCommand => new (() =>
+        {
+            if (DatabaseManager == null)
+            {
+                var dbContext = new DatabaseContext();
+                dbContext.Database.EnsureCreated();
+                DatabaseManager = new DatabaseManager(dbContext);
+            }
+
+            Comments = new ObservableCollection<Comment>(DatabaseManager.SearchComments(new SearchOption()));
+        });
     }
 }
