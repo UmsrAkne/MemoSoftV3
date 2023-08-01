7732ad37dead00bbc3b003db8c8faf0a6dd1fbc7
Akn <sibata486@gmail.com>
2023-08-01 16:27:12 +0900

    add / 日時の表示部に Converter を使用

---
+++ MemoSoftV3/Models/Converters/DateTimeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MemoSoftV3.Models.Converters
+{
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return new DateTime(0).ToString(CultureInfo.InvariantCulture);
+            }
+
+            var dateTime = (DateTime)value;
+            return dateTime.ToString("yyyy/MM/dd HH:mm");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
