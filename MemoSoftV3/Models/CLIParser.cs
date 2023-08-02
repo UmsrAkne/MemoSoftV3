027010cb393e941e82736cf745d9acc166d03c9b
Akn <sibata486@gmail.com>
2023-08-03 01:08:33 +0900

    add feat / コマンドをパースする機能を実装

--- MemoSoftV3/Models/CLIParser.cs
+++ MemoSoftV3/Models/CLIParser.cs
@@ -1,34 +1,57 @@
-﻿using CommandLine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using CommandLine;
 
 namespace MemoSoftV3.Models
 {
     public class CliParser
     {
-        public string[] SplitArg(string arg)
+        public List<string> SplitArg(string arg)
         {
-            return arg.Split(" ");
+            var sp1 = arg.Split('"');
+            List<string> sps = new ();
+            for (var i = 0; i < sp1.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    sps.Add(sp1[i]);
+                }
+                else
+                {
+                    sps.AddRange(sp1[i].Split(" "));
+                }
+            }
+
+            sps = sps.Where(s => !string.IsNullOrEmpty(s)).ToList();
+
+            return sps;
         }
 
-        public void Parse(string argString)
+        public CliOption Parse(string argString)
         {
             var args = SplitArg(argString);
             var parseResult = Parser.Default.ParseArguments<CliOption>(args);
+
+            var opt = new CliOption();
 
             switch (parseResult.Tag)
             {
                 // パース成功
                 case ParserResultType.Parsed:
-                    var parsed = parseResult as Parsed<CliOption>;
-                    var opt = parsed.Value;
+                    if (parseResult is Parsed<CliOption> parsed)
+                    {
+                        opt = parsed.Value;
+                        opt.Tags = opt.Tags.Select(s => s.Replace(",", string.Empty));
+                    }
+
                     break;
 
                 // パース失敗
                 case ParserResultType.NotParsed:
-                    // パースの成否でパース結果のオブジェクトの方が変わる
-                    var notParsed = parseResult as NotParsed<CliOption>;
-
                     break;
             }
+
+            return opt;
         }
     }
 }
--- MemoSoftV3Tests/Models/CliParserTest.cs
+++ MemoSoftV3Tests/Models/CliParserTest.cs
@@ -9,8 +9,30 @@ namespace MemoSoftV3Tests.Models
         public void SplitArgTest()
         {
             var parser = new CliParser();
-            var args = "-t aaa,bbb,ccc -g testGroup -f ";
-            var a = parser.SplitArg(args);
+            var args = "-t \"aaa bbb\",ccc -g testGroup -f ";
+            var a = parser.SplitArg(args).ToList();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(a[0], Is.EqualTo("-t"));
+                Assert.That(a[1], Is.EqualTo("aaa bbb"));
+                Assert.That(a[2], Is.EqualTo(",ccc"));
+                Assert.That(a[3], Is.EqualTo("-g"));
+                Assert.That(a[4], Is.EqualTo("testGroup"));
+                Assert.That(a[5], Is.EqualTo("-f"));
+            });
+        }
+
+        [Test]
+        public void ParseTest()
+        {
+            var parser = new CliParser();
+            var args = "-t \"aaa bbb\",ccc -g \"test Group\" -f ";
+            var a = parser.Parse(args);
+
+            Assert.AreEqual("aaa bbb", a.Tags.ToList()[0]);
+            Assert.AreEqual("ccc", a.Tags.ToList()[1]);
+            Assert.AreEqual("test Group", a.GroupName);
         }
     }
 }
