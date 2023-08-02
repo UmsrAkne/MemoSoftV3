d154d9ce423ead69029841219b15711327f1b0ba
Akn <sibata486@gmail.com>
2023-08-02 16:06:05 +0900

    add / コマンドライン引数を解析するためのファイルを追加

---
+++ MemoSoftV3/Models/CliOption.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+
+namespace MemoSoftV3.Models
+{
+    public class CliOption
+    {
+        [Option(
+                'g',
+                "group",
+                Required = false,
+                HelpText = "コメントを追加するグループを指定します。"),
+        ]
+        public string GroupName { get; set; }
+
+        [Option(
+                'c',
+                "checkable",
+                Default = true,
+                Required = false,
+                HelpText = "チェック可能なコメントを生成します。"),
+        ]
+        public bool Checkable { get; set; }
+
+        [Option(
+                'f',
+                "favorite",
+                Default = true,
+                Required = false,
+                HelpText = "お気に入りのコメントを生成します。"),
+        ]
+        public bool IsFavorite { get; set; }
+
+        [Option(
+                't',
+                "tag",
+                Required = false,
+                Separator = ',',
+                HelpText = "タグを追加します。カンマで区切ることで、複数のタグを追加可能です。"),
+        ]
+        public IEnumerable<string> Tags { get; set; }
+    }
+}
