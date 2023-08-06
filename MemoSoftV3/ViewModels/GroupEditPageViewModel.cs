f11d3108b7a45fd488c786b04a32126267db2a18
Akn <sibata486@gmail.com>
2023-08-07 00:55:31 +0900

    add / グループの情報を編集するユーザーコントロールを追加

---
+++ MemoSoftV3/ViewModels/GroupEditPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Prism.Commands;
+using Prism.Services.Dialogs;
+
+namespace MemoSoftV3.ViewModels
+{
+    public class GroupEditPageViewModel : IDialogAware
+    {
+        public event Action<IDialogResult> RequestClose;
+
+        public string Title => string.Empty;
+
+        public DelegateCommand CloseCommand => new (() =>
+        {
+            RequestClose?.Invoke(new DialogResult());
+        });
+
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+        }
+    }
+}
---
+++ MemoSoftV3/Views/GroupEditPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MemoSoftV3.Views
+{
+    public partial class GroupEditPage : Page
+    {
+        public GroupEditPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
