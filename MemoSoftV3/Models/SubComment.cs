76f136f5d8fc6a7c7e02e00fcf7acedb58f20e9f
Akn <sibata486@gmail.com>
2023-08-15 17:22:24 +0900

    add / SubComment を BindableBase のサブクラスに設定

--- MemoSoftV3/Models/SubComment.cs
+++ MemoSoftV3/Models/SubComment.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Prism.Mvvm;
 
 namespace MemoSoftV3.Models
 {
-    public class SubComment : IDatabaseEntity
+    public class SubComment : BindableBase, IDatabaseEntity
     {
+        private TimeSpan workingTimeSpan = TimeSpan.Zero;
+        private bool timeTracking;
+
         [Key]
         [Required]
         public int Id { get; set; }
@@ -23,10 +27,10 @@ namespace MemoSoftV3.Models
         public bool Checked { get; set; }
 
         [Required]
-        public bool TimeTracking { get; set; }
+        public bool TimeTracking { get => timeTracking; set => SetProperty(ref timeTracking, value); }
 
         [NotMapped]
-        public TimeSpan WorkingTimeSpan { get; set; } = TimeSpan.Zero;
+        public TimeSpan WorkingTimeSpan { get => workingTimeSpan; set => SetProperty(ref workingTimeSpan, value); }
 
         [NotMapped]
         public DateTime DateTime { get; set; } = DateTime.Now;
