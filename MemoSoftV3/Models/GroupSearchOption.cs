153ad318a786e47f2ca7545bd893ec123e619b8e
Akn <sibata486@gmail.com>
2023-10-06 18:47:04 +0900

    add / グループ検索時のオプションとなるクラスを追加

---
+++ MemoSoftV3/Models/GroupSearchOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MemoSoftV3.Models
+{
+    public class GroupSearchOption
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public DateTime StartDateTime { get; set; } = new (0);
+
+        public DateTime EndDateTime { get; set; } = DateTime.MaxValue;
+
+        public bool ContainsArchivedGroup { get; set; } = false;
+    }
+}
